c7c1600678b6ff675a3533fbe1e828f3589e93bc
yuya737 <kawakami@grinnell.edu>
2019-07-25 12:57:03 -0400

    things

--- myhereapp/main.py
+++ myhereapp/main.py
@@ -86,7 +86,7 @@ def singleGPUCompute(graph):
     graph = figure(plot_width=800, plot_height=400, y_axis_label='time (ms)', tools=tools_to_show, y_axis_type="log")
     graph.xaxis.ticker = FixedTicker(ticks=[160,320,640,960,1280,1600,1920])
     setHover(graph)
-    singleGPUCompute = pd.read_csv("Data/singleGPUCompute.csv")
+    singleGPUCompute = pd.read_csv("singleGPUCompute.csv")
     names = [singleGPUCompute.columns[i] for i in range(1,5)]
     for i in range(4):
         source = ColumnDataSource(data={
@@ -108,7 +108,7 @@ def singleGPUData(graph):
     graph = figure(plot_width=800, plot_height=400, y_axis_label='time (ms)', tools=tools_to_show, y_axis_type="log")
     graph.xaxis.ticker = FixedTicker(ticks=[160,320,640,960,1280,1600,1920])
     setHover(graph)
-    singleGPUData = pd.read_csv("Data/singleGPUData.csv")
+    singleGPUData = pd.read_csv("singleGPUData.csv")
     names = [singleGPUData.columns[i] for i in range(1,5)]
     for i in range(4):
         source = ColumnDataSource(data={
@@ -130,7 +130,7 @@ def multiGPUCompute(graph):
     graph = figure(plot_width=800, plot_height=400, y_axis_label='time (ms)', tools=tools_to_show)
     graph.xaxis.ticker = FixedTicker(ticks=[160,320,640,960,1280,1600,1920,2240,2560,2880])
     setHover(graph)
-    multiGPUCompute = pd.read_csv("Data/multiGPUCompute.csv")
+    multiGPUCompute = pd.read_csv("multiGPUCompute.csv")
     names = [multiGPUCompute.columns[i] for i in range(1,3)]
 
     for i in range(2):
@@ -153,7 +153,7 @@ def multiGPUData(graph):
     graph = figure(plot_width=800, plot_height=400, y_axis_label='time (ms)', tools=tools_to_show)
     graph.xaxis.ticker = FixedTicker(ticks=[160,320,640,960,1280,1600,1920,2240,2560,2880])
     setHover(graph)
-    multiGPUData = pd.read_csv("/Data/multiGPUData.csv")
+    multiGPUData = pd.read_csv("multiGPUData.csv")
     names = [multiGPUData.columns[i] for i in range(1,3)]
 
     for i in range(2):
